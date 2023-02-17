54febcbafb91bcda1b40c94a675068f51e76c4c4
Kaanayden <kaanayden15@gmail.com>
2023-02-17 22:52:13 +0300

    Reinitialize repo

---
+++ ulaknetapp/App.jsx
@@ -0,0 +1,63 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+
+
+function App() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+
+  return (
+    <SafeAreaView >
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic">
+
+        
+        <View>
+        <Text>ULAK NET</Text>
+
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
