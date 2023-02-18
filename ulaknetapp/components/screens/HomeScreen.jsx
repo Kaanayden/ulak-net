4825dadd02a8d89f6a314e82efc5213f0cde878a
Kaanayden <kaanayden15@gmail.com>
2023-02-18 19:09:24 +0300

    Fix vector icons

--- ulaknetapp/components/screens/HomeScreen.jsx
+++ ulaknetapp/components/screens/HomeScreen.jsx
@@ -3,10 +3,14 @@ import {
   View,
 } from 'react-native';
 
+import Icon from 'react-native-vector-icons/FontAwesome';
+const myIcon = <Icon name="rocket" size={30} color="#900" />;
+
 function HomeScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Home!</Text>
+      {myIcon}
     </View>
   );
 }
